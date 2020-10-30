15dc8f9fb00fafc349bc04d8e3efc44586cf28b3
elpintozo <ricardo.sr.lepe@gmail.com>
2020-10-30 01:26:05 -0300

    [feat][models][se agregan los primeros modelos, aún no modelados del todo]

--- apps/menus/models.py
+++ apps/menus/models.py
@@ -1,3 +1,18 @@
+# Django
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Preference(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+
+# class User(AbstractUser):
+#
+#     def get_full_name(self):
+#         return '{} {}'.format(self.first_name, self.last_name)
+#
+# class Employee(User):
+#     create_at = models.DateTimeField(auto_now_add=True)
+#
+#     #Indico que será un proxy model
+#     class Meta:
+#         proxy = True
