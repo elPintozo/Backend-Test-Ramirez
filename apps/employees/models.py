efaa60cd8f688ce7e4d3988fdab8bf94944f475a
RicardoR <ricardo.ramirez@manager.cl>
2020-10-30 18:12:27 -0300

    [fix][models][se crearon por error models donde no se debian]

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -1,34 +1,9 @@
-# Django and Python
-import uuid
+# Django
 from django.db import models
-from django.db.models.signals import pre_save
+from django.contrib.auth.models import AbstractUser
 
-class Ingredient(models.Model):
+class Preference(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
 
-class Plate(models.Model):
+class Employee(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
-
-class Menu(models.Model):
-    menu_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.menu_id
-
-def set_menu_id(sender, instance, *args, **kwargs):
-    """
-    Function that help me to create diffucult ID for the menu instance
-    :param sender (Class): Menu class
-    :param instance (Object): Menu instace
-    :param args (list): other paramethers
-    :param kwargs (list): other paramethers
-    :return (None):
-    """
-    #valid if menu has id
-    if not instance.menu_id:
-        #add strong id
-        instance.menu_id = str(uuid.uuid4())
-
-#I notific that before create menu new, must pass for particular function
-pre_save.connect(set_menu_id, sender=Menu)
